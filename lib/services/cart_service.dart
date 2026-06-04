75f77f6678697464816af9fd8c5459cd38594d23
prasiddha <prasiddhadahal33@gmail.com>
2026-06-04 07:56:10 +0545

    cart model and service added

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,14 +1,22 @@
 import 'package:flutterecommerce/controllers/storage_controller.dart';
+import 'package:flutterecommerce/utils/apiUtil.dart';
 import 'package:get/get.dart';
 
 class CartService {
-  static Future addToCart() async{
+  static Future addToCart(int productId, int quantity) async {
     // get token first
 
     var controller = Get.find<StorageController>();
 
     final token = controller.getToken();
 
+    // add token to the dio header
+    Apiutil.dio.options.headers["Authorization"] = "Bearer $token";
 
+    var response = await Apiutil.dio.post(
+      "/cart",
+      data: {"product_id": productId, "qty": quantity},
+    );
+    return response;
   }
 }
